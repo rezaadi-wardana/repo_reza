c20c131a7319de6bcfd37ae7a3672eec380c683e
REZA <wardhana15.aw@gmail.com>
2023-03-28 08:58:32 +0700

    menambahkan tipedata string, null, boolean, symbol

--- tipedata.js
+++ tipedata.js
@@ -12,3 +12,24 @@ console.log(typeof(angka));
 var word = "ini tipe data string";
 document.write(word);
 console.log(typeof(word));
+
+//null
+let tiada = null;
+console.log(tiada);
+
+//Boolean
+const a = 1;
+const b = 2;
+
+let hasilnyabenar = a < b;
+let hasilnyasalah = a > b;
+
+console.log("ini nilai boolean", hasilnyabenar);
+console.log("ini nilai boolean", hasilnyasalah);
+
+//Symbol
+const id1 = Symbol("id");
+const id2 = Symbol("id");
+
+console.log(id1, id2);
+
