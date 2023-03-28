fbea16f08c03cfea44ff91f9ab8748111f5081fb
REZA <wardhana15.aw@gmail.com>
2023-03-28 08:49:10 +0700

    menambahkan String

--- tipedata.js
+++ tipedata.js
@@ -1,9 +1,14 @@
 //undefined
 var kosong;
 console.log(kosong);
+console.log(typeof(kosong));
 
 //number 
 var angka = 1;
 console.log(angka);
+console.log(typeof(angka));
 
-//
+//string 
+var word = "ini tipe data string";
+document.write(word);
+console.log(typeof(word));
