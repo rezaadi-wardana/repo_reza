748245d94d2d66d227d00941be4edca909fa15a5
REZA <wardhana15.aw@gmail.com>
2023-03-28 07:58:10 +0700

    Operator Aritmatika

---
+++ operator.js
@@ -0,0 +1,20 @@
+//penambahan
+x = 2;
+y = 4;
+z = x + y;
+console.log(z);
+//pengurangan
+x = 20;
+y = 4;
+z = x - y;
+console.log(z);
+//perkalian
+x = 2;
+y = 4;
+z = x * y;
+console.log(z);
+//pembagian
+x = 2;
+y = 4;
+z = x : y;
+console.log(z);
