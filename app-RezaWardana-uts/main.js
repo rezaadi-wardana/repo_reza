a7d39069e3d7a0e0fc56c2cf31d8b09e440f3456
REZA <wardhana15.aw@gmail.com>
2023-05-16 13:36:02 +0700

    menambahkan Tugas UTS

---
+++ app-RezaWardana-uts/main.js
@@ -0,0 +1,8 @@
+var btn_nama = document.getElementaryById("nama");
+let btn_nim = document.getElementaryById("nim");
+let btn_matkul = document.getElementaryById("matkul");
+let btn_nilai = document.getElementaryById("nilai");
+
+function tampilkan(){
+    btn_nama
+}
