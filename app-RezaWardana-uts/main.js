aea1851cc131f7f659ff2b81ae5afdbf2545d45b
rezaadi-wardana <wardana15.aw@gmail.com>
2023-05-18 12:15:59 +0700

    mengedit program js

--- app-RezaWardana-uts/main.js
+++ app-RezaWardana-uts/main.js
@@ -1,8 +1,73 @@
-var btn_nama = document.getElementaryById("nama");
-let btn_nim = document.getElementaryById("nim");
-let btn_matkul = document.getElementaryById("matkul");
-let btn_nilai = document.getElementaryById("nilai");
+const arraySatu = [];
 
-function tampilkan(){
-    btn_nama
+daftar.onsubmit = async (e) => {
+    e.preventDefault();
+
+    let data = new FormData(daftar);
+
+    let object = {}
+    data.forEach((val,key) => {
+        object[key] = val 
+        const rata = (Number(object['uas']) + Number(object['uts']) + Number(object['tugas'])) / 3;
+        let huruf;
+        let keterangan;
+        if (rata >= 80){
+            huruf = 'A';
+            keterangan = 'Lulus';
+        }
+        else if (rata >= 70){
+            huruf = 'B';
+            keterangan = 'Lulus';
+        }
+        else if (rata >= 60){
+            huruf = 'C';
+            keterangan = 'Lulus';
+        }
+        else if (rata >= 50){
+            huruf = 'D';
+            keterangan = 'Tidak Lulus';
+        }
+        else if (rata >= 50){
+            huruf = 'E';
+            keterangan = 'Tidak Lulus';
+        }
+        object['rata'] = rata;
+        object['huruf'] = huruf;
+        object['keterangan'] = keterangan;
+    });
+    arraySatu.push(object);
+
+    let outputHTML = "";
+    outputHTML += "<table>";
+    outputHTML += "<tr>";
+    outputHTML += "<th>No.</th>"
+    outputHTML += "<th>NIM</th>"
+    outputHTML += "<th>Nama.</th>"
+    outputHTML += "<th>Rata</th>"
+    outputHTML += "<th>Huruf</th>"
+    outputHTML += "<th>keterangan</th>"
+    outputHTML += "</tr>"
+
+    let no = 1;
+    arraySatu.forEach((val) => {
+        outputHTML += "<tr>";
+        outputHTML += "<td>"+ no++ + "</td>";
+        outputHTML += "<td>"+ val.nim + "</td>";
+        outputHTML += "<td>"+ val.nama + "</td>";
+        outputHTML += "<td>"+ val.rata + "</td>";
+        outputHTML += "<td>"+ val.huruf + "</td>";
+        outputHTML += "<td>"+ val.keterangan + "</td>";
+        outputHTML += "</tr>";
+
+        
+    });
+    outputHTML += "</table>"
+
+    document.getElementById("tdaftar").innerHTML = outputHTML;
 }
+
+// var btn_nama = document.getElementById("nama");
+// let btn_nim = document.getElementById("nim");
+// let btn_matkul = document.getElementById("matkul");
+// let btn_nilai = document.getElementById("nilai");
+
