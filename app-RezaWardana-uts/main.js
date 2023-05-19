ca3598cf736ae0821219f850ed7e6ab37c43e47d
rezaadi-wardana <wardana15.aw@gmail.com>
2023-05-19 11:17:30 +0700

    menambahkan biodata dan data array

--- app-RezaWardana-uts/main.js
+++ app-RezaWardana-uts/main.js
@@ -8,31 +8,58 @@ daftar.onsubmit = async (e) => {
     let object = {}
     data.forEach((val,key) => {
         object[key] = val 
-        const rata = (Number(object['uas']) + Number(object['uts']) + Number(object['tugas'])) / 3;
+        const nilaiAkhir = (Number(object['nilaiAkhir']));
         let huruf;
+        let indeks;
         let keterangan;
-        if (rata >= 80){
+        if (nilaiAkhir >= 85 && nilaiAkhir <= 100){
             huruf = 'A';
+            indeks = 4.00;
             keterangan = 'Lulus';
         }
-        else if (rata >= 70){
+        else if (nilaiAkhir >= 79 && nilaiAkhir <85){
+            huruf = 'A-';
+            indeks = 3.67;
+            keterangan = 'Lulus';
+        }
+        else if (nilaiAkhir >= 74 && nilaiAkhir <79){
+            huruf = 'B+';
+            indeks = 3.33;
+            keterangan = 'Lulus';
+        }
+        else if (nilaiAkhir >= 69 && nilaiAkhir <74){
             huruf = 'B';
+            indeks = 3.00;
             keterangan = 'Lulus';
         }
-        else if (rata >= 60){
+        else if (nilaiAkhir >= 64 && nilaiAkhir <69){
+            huruf = 'B-';
+            indeks = 2.67;
+            keterangan = 'Lulus';
+        }
+        else if (nilaiAkhir >= 59 && nilaiAkhir <64){
+            huruf = 'C+';
+            indeks = 2.33;
+            keterangan = 'Lulus';
+        }
+        else if (nilaiAkhir >= 54 && nilaiAkhir <59){
             huruf = 'C';
+            indeks = 2.00;
             keterangan = 'Lulus';
         }
-        else if (rata >= 50){
+        else if (nilaiAkhir >= 41 && nilaiAkhir <54){
             huruf = 'D';
+            indeks = 1.00;
             keterangan = 'Tidak Lulus';
         }
-        else if (rata >= 50){
+        else if (nilaiAkhir >= 0 && nilaiAkhir <41){
             huruf = 'E';
+            indeks = 0.00;
             keterangan = 'Tidak Lulus';
         }
-        object['rata'] = rata;
+        object['nilaiAkhir'] = nilaiAkhir;
         object['huruf'] = huruf;
+        object['indeks'] = indeks;
         object['keterangan'] = keterangan;
     });
     arraySatu.push(object);
@@ -42,10 +69,12 @@ daftar.onsubmit = async (e) => {
     outputHTML += "<tr>";
     outputHTML += "<th>No.</th>"
     outputHTML += "<th>NIM</th>"
-    outputHTML += "<th>Nama.</th>"
-    outputHTML += "<th>Rata</th>"
+    outputHTML += "<th>Nama</th>"
+    outputHTML += "<th>Matkul</th>"
+    outputHTML += "<th>Nilai Angka</th>"
     outputHTML += "<th>Huruf</th>"
-    outputHTML += "<th>keterangan</th>"
+    outputHTML += "<th>Indeks</th>"
+    outputHTML += "<th>Keterangan</th>"
     outputHTML += "</tr>"
 
     let no = 1;
@@ -53,9 +82,11 @@ daftar.onsubmit = async (e) => {
         outputHTML += "<tr>";
         outputHTML += "<td>"+ no++ + "</td>";
         outputHTML += "<td>"+ val.nim + "</td>";
-        outputHTML += "<td>"+ val.nama + "</td>";
-        outputHTML += "<td>"+ val.rata + "</td>";
+        outputHTML += "<td>"+ val.name + "</td>";
+        outputHTML += "<td>"+ val.matkul + "</td>";
+        outputHTML += "<td>"+ val.nilaiAkhir + "</td>";
         outputHTML += "<td>"+ val.huruf + "</td>";
+        outputHTML += "<td>"+ val.indeks + "</td>";
         outputHTML += "<td>"+ val.keterangan + "</td>";
         outputHTML += "</tr>";
 
@@ -64,6 +95,11 @@ daftar.onsubmit = async (e) => {
     outputHTML += "</table>"
 
     document.getElementById("tdaftar").innerHTML = outputHTML;
+    
+    //DATA ARRAY
+    const dataArray = [];
+
+    
 }
 
 // var btn_nama = document.getElementById("nama");
