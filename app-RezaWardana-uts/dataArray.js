95bc9a741feef0cd63e098b07325fee755aa3c64
rezaadi-wardana <wardana15.aw@gmail.com>
2023-05-19 14:52:45 +0700

    memanipulasi array

---
+++ app-RezaWardana-uts/dataArray.js
@@ -0,0 +1,16 @@
+//DATA ARRAY
+let dataMahasiswa = [];
+let mahasiswa1 = ["Muhammad Reza",2004,089516628719];
+let mahasiswa2 = ["Reza Adi",2003,08982194477];
+let mahasiswa3 = ["Adi Wardana",2004,089615645470];
+let mahasiswa4 = ["Reza Wardana",2024,089123456789];
+dataMahasiswa.push(mahasiswa1,mahasiswa2,mahasiswa3,mahasiswa4);
+
+const keterangan = ["Nama Mahasiswa","Tahun Lahir", "Nomor Handphone"];
+dataMahasiswa.unshift(keterangan);
+
+dataMahasiswa.pop(mahasiswa4);//Belum Lahir
+
+for(const arrayItem of dataMahasiswa){
+    document.writeln(arrayItem+"<br>");
+}
